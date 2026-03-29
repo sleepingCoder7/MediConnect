bf6757e199ee27894b5694d9da8888f10940c822
java_nov_2024-admin <durgaprasadkar1998.dp@gmail.com>
2026-03-29 12:12:39 +0530

    Added validation for phone numbers

--- frontend/src/pages/BookAppointment.jsx
+++ frontend/src/pages/BookAppointment.jsx
@@ -18,8 +18,9 @@ const BookAppointment = () => {
         comments: '',
         reports: []
     });
-    const inputClass = "w-full p-2 mb-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent cursor-pointer";
+    const inputClass = "w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-accent cursor-pointer";
     const [departments, setDepartments] = useState([]);
+    const [errors, setErrors] = useState({});
 
     useEffect(() => {
         fetchDepartments();
@@ -57,8 +58,27 @@ const BookAppointment = () => {
         setFormValues({...formValues, date: e.target.value});
     };
 
+    const handlePhoneChange = (e) => {
+        setFormValues({...formValues, phone: e.target.value});
+        const value = e.target.value;
+        if (!/^[0-9]*$/.test(value)) {
+            setErrors({...errors, "phone": "Phone number must contain only numbers"});
+            return;
+        }
+        if (value.length < 10) {
+            setErrors({...errors, "phone": "Phone number must be at least 10 digits"});
+            return;
+        }
+        const {phone, ...remainingErrors} = errors;
+        setErrors(remainingErrors);
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if(Object.keys(errors).length > 0){
+            toast.error("Please fix the errors before submitting", {id: "bookAppointment"});
+            return;
+        }
         toast.loading("Booking appointment...", {id: "bookAppointment"});
         
         try{
@@ -92,13 +112,13 @@ const BookAppointment = () => {
         <main className='flex justify-center p-6 w-full'>
             <form className='bg-white p-6 rounded shadow-md w-full md:w-[480px] flex flex-col h-fit md:mt-10' onSubmit={handleSubmit}>
                 <h2 className='text-xl font-semibold text-primary text-center mb-6'>Book an Appointment</h2>
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-2 mt-2'>
                     <FiCalendar size={16} color='#007bff' />
                     <label htmlFor="date">Select Date and time</label>
                 </div>
                 <input type="datetime-local" id="date" name="date" required min={getMinDateTime()} value={formValues.date} onChange={handleDateChange} className={inputClass} />
 
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-2 mt-4'>
                     <FcDepartment size={16} color='#007bff' />
                     <label htmlFor="department">Select Department</label>
                 </div>
@@ -109,19 +129,20 @@ const BookAppointment = () => {
                     ))}
                 </select>
 
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-2 mt-4'>
                     <FiPhone size={16} color='#007bff' />
                     <label htmlFor="phone">Phone Number</label>
                 </div>
-                <input type="text" id="phone" name="phone" maxLength={10} placeholder="Enter your phone number" required value={formValues.phone} onChange={handleChange} className={inputClass}/>
+                <input type="text" id="phone" name="phone" minLength={10} maxLength={10} placeholder="Enter your phone number" required value={formValues.phone} onChange={handlePhoneChange} className={inputClass}/>
+                {errors.phone && <p className="text-red-500 text-sm">{errors.phone}</p>}
 
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-2 mt-4'>
                     <FiMessageSquare size={16} color='#007bff' />
                     <label htmlFor="comments">Comments</label>
                 </div>
                 <textarea id="comments" name="comments" placeholder="Explain your symptoms" rows="4" value={formValues.comments} onChange={handleChange} className={inputClass}></textarea>
 
-                <div className='flex items-center gap-2'>
+                <div className='flex items-center gap-2 mt-4'>
                     <FiUpload size={16} color='#007bff' />
                     <label htmlFor="reports">Upload Reports</label>
                 </div>
